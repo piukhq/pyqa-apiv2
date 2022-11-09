5718ca19e6ccc4d36280fd698ed1da26f2a73e0f
braghavanbink <braghavan@bink.com>
2022-11-09 14:23:00 +0000

    updated the trusted channel add endpoint

--- tests/api/__init__.py
+++ tests/api/__init__.py
@@ -9,7 +9,7 @@ ENDPOINT_LOYALTY_PLANS_OVERVIEW = "/v2/loyalty_plans_overview"
 ENDPOINT_LOYALTY_PLAN_DETAILS = "/v2/loyalty_plans/{}/plan_details"
 
 ENDPOINT_MEMBERSHIP_CARDS_ADD = "/v2/loyalty_cards/add"
-ENDPOINT_MEMBERSHIP_CARDS_ADD_TRUSTED = "/v2/loyalty_cards/add"
+ENDPOINT_MEMBERSHIP_CARDS_ADD_TRUSTED = "/v2/loyalty_cards/trusted_add"
 ENDPOINT_MEMBERSHIP_CARD = "/v2/loyalty_cards/add/{}"
 
 ENDPOINT_MEMBERSHIP_CARDS_AUTHORISE = "/v2/loyalty_cards/{}/authorise"
