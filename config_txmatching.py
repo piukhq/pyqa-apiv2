1b8ee36ecc992fb67095daefa78a67c23c156b95
navyaannajames <njames@bink.com>
2023-03-01 13:42:13 +0000

    updated isort & black

--- config_txmatching.py
+++ config_txmatching.py
@@ -8,24 +8,19 @@ class EnvironmentDetails:
 
 
 if "KUBERNETES_SERVICE_HOST" in environ:
-
     STAGING = EnvironmentDetails(
-        transaction_matching_base_url="http://skiron-api",
-        transaction_matching_base_url_zephyrus="http://zephyrus-api"
+        transaction_matching_base_url="http://skiron-api", transaction_matching_base_url_zephyrus="http://zephyrus-api"
     )
 
     SANDBOX = EnvironmentDetails(
-        transaction_matching_base_url="http://skiron-api",
-        transaction_matching_base_url_zephyrus="http://zephyrus-api"
+        transaction_matching_base_url="http://skiron-api", transaction_matching_base_url_zephyrus="http://zephyrus-api"
     )
 
     PROD = EnvironmentDetails(
-        transaction_matching_base_url="http://skiron-api",
-        transaction_matching_base_url_zephyrus="http://zephyrus-api"
+        transaction_matching_base_url="http://skiron-api", transaction_matching_base_url_zephyrus="http://zephyrus-api"
     )
 else:
-
     STAGING = EnvironmentDetails(
         transaction_matching_base_url="https://api.staging.gb.bink.com",
-        transaction_matching_base_url_zephyrus="https://api.staging.gb.bink.com"
+        transaction_matching_base_url_zephyrus="https://api.staging.gb.bink.com",
     )
