7dc11e8547ca5046b38c996b31826025b4cbe021
Thenuja Viknarajah <thenuja_1@outlook.com>
2023-04-11 13:12:24 +0100

    generalise script

--- report-render.sh
+++ report-render.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-bolb_name=$(date +%s | awk '{ print strftime("%Y%m%d-%H%M", $1);  }')-$(echo $RANDOM | md5sum | head -c 6).html
-url="https://binkuksouthstaging.blob.core.windows.net/qareports/pytest_report/apiv2-${bolb_name}"
+BlobName=$(date +%s | awk '{ print strftime("%Y%m%d-%H%M", $1);  }')-$(echo $RANDOM | md5sum | head -c 6).html
+AccountName=$(echo $BLOB_STORAGE_DSN | awk -F ';' '{print $2}' | sed 's/AccountName=//g')
+AccountKey=$(echo $BLOB_STORAGE_DSN | awk -F ';' '{print $3}' | sed 's/AccountKey=//g')
+EndpointSuffix=$(echo $BLOB_STORAGE_DSN | awk -F ';' '{print $4}' | sed 's/EndpointSuffix=//g')
+ReportPath=$REPORT_DIRECTORY
+ContainerName=$REPORT_CONTAINER
+url="https://${AccountName}.blob.${EndpointSuffix}/${ContainerName}/${ReportPath}-${BlobName}"
 
 # look for report.html generated during pytest run
 while [ ! -f /tmp/report.html ]; do
@@ -10,7 +15,7 @@ done
 sleep 2
 
 # copy report to azureblob
-az storage blob upload --account-name $(echo $BLOB_STORAGE_DSN | awk -F ';' '{print $2}' | sed 's/AccountName=//g') --container-name qareports --name "pytest_report/apiv2-$bolb_name" --file /tmp/report.html --account-key $(echo $BLOB_STORAGE_DSN | awk -F ';' '{print $3}' | sed 's/AccountKey=//g') --auth-mode key
+az storage blob upload --account-name $AccountName --container-name $ContainerName --name "$ReportPath-$BlobName" --file /tmp/report.html --account-key $AccountKey --auth-mode key
 
 # determine what message to POST to teams using the error.log
 if
