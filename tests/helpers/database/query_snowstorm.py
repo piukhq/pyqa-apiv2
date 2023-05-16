8310f861180853ffdd393fad244e1744b44f5660
RupalPatelj <rpatel@bink.com>
2023-05-16 13:54:35 +0100

    Event join loyaltycard

--- tests/helpers/database/query_snowstorm.py
+++ tests/helpers/database/query_snowstorm.py
@@ -35,21 +35,8 @@ def get_user_created_event(journey_type, external_id):
     if journey_type == "":
         logging.info("Scheme did not attach to the wallet")
     elif TestContext.environ == "staging":
-        if journey_type == "user_created":
-            query_event_record = (
-                """SELECT id, event_date_time, event_type, json
-                           FROM snowstorm.public.events
-                           WHERE event_type = 'user.created'
-                           AND json ->> 'external_user_ref' = '%s'"""
-                % external_id
-            )
-        elif journey_type == "user_deleted":
-            query_event_record = (
-                """SELECT id, event_date_time, event_type, json
+        query_event_record = f"""SELECT id, event_date_time, event_type, json
                                FROM snowstorm.public.events
-                               WHERE event_type = 'user.deleted'
-                               AND json ->> 'external_user_ref' = '%s'"""
-                % external_id
-            )
-
+                               WHERE event_type = '{journey_type}'
+                               AND json ->> 'external_user_ref' = '{external_id}' ORDER BY event_date_time DESC"""
     return query_event_record
